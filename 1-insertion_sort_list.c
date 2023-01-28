9c6ae340ee86dd62e4426bc750c1a2bcf4c9f438
solomon1227 <solomonf1227@gmail.com>
2023-01-28 23:05:04 +0300

    sorting doubly linked list using insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+  * insertion_sort - sort a doubly linked list in insertion sort algorithm
+  * @list: doubly linked list to be sorted
+  */
+void insertion_sort_list(listint_t **list)
+{
+	for (i = 1; list->next; listint_t *current, *swap, *prv;
+
+	if (!list || !*list)
+		return;
+
+	current = *list;
+	while ((current = current->next))
+	{
+		swap = current;
+		while (swap->prev && swap->n < swap->prev->n)
+		{
+			prv = swap->prev;
+			if (swap->next)
+				swap->next->prev = prv;
+			if (prv->prev)
+				prv->prev->next = swap;
+			else
+				*list = swap;
+			prv->next = swap->next;
+			swap->prev = prv->prev;
+			swap->next = prv;
+			prv->prev = swap;
+
+			print_list(*list);
+		}
+	}
+}
